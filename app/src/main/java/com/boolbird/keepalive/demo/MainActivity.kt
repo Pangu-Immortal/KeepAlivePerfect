f2b28c94a39a1b54e2f7afd470277406df4c5071
齐浩 <hao.qi@transsnet.com>
2023-06-28 16:50:12 +0800

    适配Android14;

--- app/src/main/java/com/boolbird/keepalive/demo/MainActivity.kt
+++ app/src/main/java/com/boolbird/keepalive/demo/MainActivity.kt
@@ -1,7 +1,7 @@
-package com.boolbird.keepalive.demo;
+package com.boolbird.keepalive.demo
 
-import android.app.Activity;
-import android.os.Bundle;
+import android.app.Activity
+import android.os.Bundle
 
 /**
  * Doc说明 (此类核心功能):
@@ -9,27 +9,27 @@ import android.os.Bundle;
  * | @author qihao             |
  * | @date on 2021/5/10 15:28 |
  * +---------------------------+
- *  ┌─────────────────────────────────────────────────────────────┐
- *  │┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐│
- *  ││Esc│!1 │@2 │#3 │$4 │%5 │^6 │&7 │*8 │(9 │)0 │_- │+= │|\ │`~ ││
- *  │├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┤│
- *  ││ Tab │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │{[ │}] │ BS  ││
- *  │├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤│
- *  ││ Ctrl │ A │ S │ D │ F │ G │ H │ J │ K │ L │: ;│" '│ Enter  ││
- *  │├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┬───┤│
- *  ││ Shift  │ Z │ X │ C │ V │ B │ N │ M │< ,│> .│? /│Shift │Fn ││
- *  │└─────┬──┴┬──┴──┬┴───┴───┴───┴───┴───┴──┬┴───┴┬──┴┬─────┴───┘│
- *  │      │Fn │ Alt │         Space         │ Alt │Win│   qihao  │
- *  │      └───┴─────┴───────────────────────┴─────┴───┘          │
- *  └─────────────────────────────────────────────────────────────┘
+ * ┌─────────────────────────────────────────────────────────────┐
+ * │┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐│
+ * ││Esc│!1 │@2 │#3 │$4 │%5 │^6 │&7 │*8 │(9 │)0 │_- │+= │|\ │`~ ││
+ * │├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┤│
+ * ││ Tab │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │{[ │}] │ BS  ││
+ * │├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤│
+ * ││ Ctrl │ A │ S │ D │ F │ G │ H │ J │ K │ L │: ;│" '│ Enter  ││
+ * │├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┬───┤│
+ * ││ Shift  │ Z │ X │ C │ V │ B │ N │ M │< ,│> .│? /│Shift │Fn ││
+ * │└─────┬──┴┬──┴──┬┴───┴───┴───┴───┴───┴──┬┴───┴┬──┴┬─────┴───┘│
+ * │      │Fn │ Alt │         Space         │ Alt │Win│   qihao  │
+ * │      └───┴─────┴───────────────────────┴─────┴───┘          │
+ * └─────────────────────────────────────────────────────────────┘
  *
  */
-public class MainActivity extends Activity {
+private const val TAG = "MainActivity"
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
 //        startService(new Intent(MainActivity.this, Service1.class));
     }
--- app/src/main/java/com/boolbird/keepalive/demo/MainApplication.kt
+++ app/src/main/java/com/boolbird/keepalive/demo/MainApplication.kt
@@ -1,14 +1,11 @@
-package com.boolbird.keepalive.demo;
+package com.boolbird.keepalive.demo
 
-import android.app.Application;
-import android.content.Context;
-import android.content.Intent;
-import android.util.Log;
-
-import com.fanjun.keeplive.KeepLive;
-import com.fanjun.keeplive.config.ForegroundNotification;
-import com.fanjun.keeplive.config.ForegroundNotificationClickListener;
-import com.fanjun.keeplive.config.KeepLiveService;
+import android.app.Application
+import android.content.Context
+import android.util.Log
+import com.fanjun.keeplive.KeepLive
+import com.fanjun.keeplive.config.ForegroundNotification
+import com.fanjun.keeplive.config.KeepLiveService
 
 /**
  * Doc说明 (此类核心功能):
@@ -16,59 +13,59 @@ import com.fanjun.keeplive.config.KeepLiveService;
  * | @author qihao             |
  * | @date on 2021/5/10 15:28 |
  * +---------------------------+
- *  ┌─────────────────────────────────────────────────────────────┐
- *  │┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐│
- *  ││Esc│!1 │@2 │#3 │$4 │%5 │^6 │&7 │*8 │(9 │)0 │_- │+= │|\ │`~ ││
- *  │├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┤│
- *  ││ Tab │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │{[ │}] │ BS  ││
- *  │├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤│
- *  ││ Ctrl │ A │ S │ D │ F │ G │ H │ J │ K │ L │: ;│" '│ Enter  ││
- *  │├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┬───┤│
- *  ││ Shift  │ Z │ X │ C │ V │ B │ N │ M │< ,│> .│? /│Shift │Fn ││
- *  │└─────┬──┴┬──┴──┬┴───┴───┴───┴───┴───┴──┬┴───┴┬──┴┬─────┴───┘│
- *  │      │Fn │ Alt │         Space         │ Alt │Win│   qihao  │
- *  │      └───┴─────┴───────────────────────┴─────┴───┘          │
- *  └─────────────────────────────────────────────────────────────┘
+ * ┌─────────────────────────────────────────────────────────────┐
+ * │┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐│
+ * ││Esc│!1 │@2 │#3 │$4 │%5 │^6 │&7 │*8 │(9 │)0 │_- │+= │|\ │`~ ││
+ * │├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┤│
+ * ││ Tab │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │{[ │}] │ BS  ││
+ * │├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤│
+ * ││ Ctrl │ A │ S │ D │ F │ G │ H │ J │ K │ L │: ;│" '│ Enter  ││
+ * │├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┬───┤│
+ * ││ Shift  │ Z │ X │ C │ V │ B │ N │ M │< ,│> .│? /│Shift │Fn ││
+ * │├──────┬─┴┬──┴──┬┴───┴───┴───┴───┴───┴──┬┴───┴┬──┴┬─────┴───┘│
+ * ││option│Fn│ Alt │         Space         │ Alt │Win│   qihao  │
+ * │└──────┴──┴─────┴───────────────────────┴─────┴───┘          │
+ * └─────────────────────────────────────────────────────────────┘
  *
  */
-public class MainApplication extends Application {
-    private static final String TAG = "MainApplication";
 
-    @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(base);
-        Log.d(TAG, "attachBaseContext");
+private const val TAG = "MainApplication"
 
-        //定义前台服务的默认样式。即标题、描述和图标
-        ForegroundNotification foregroundNotification = new ForegroundNotification("测试","描述", R.mipmap.ic_launcher,
-                //定义前台服务的通知点击事件
-                new ForegroundNotificationClickListener() {
+class MainApplication : Application() {
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
 
-                    @Override
-                    public void foregroundNotificationClick(Context context, Intent intent) {
-                    }
-                });
+        Log.d(TAG, "attachBaseContext")
+
+
+        // 定义前台通知的默认样式
+        val foregroundNotification = ForegroundNotification(
+            "标题", "描述", R.drawable.ic_small_notification
+        ) { context, intent ->
+            // 定义前台服务的通知点击事件
+
+        }
         //启动保活服务
-        KeepLive.startWork(this, KeepLive.RunMode.ENERGY, foregroundNotification,
-                //你需要保活的服务，如socket连接、定时任务等，建议不用匿名内部类的方式在这里写
-                new KeepLiveService() {
-                    /**
-                     * 运行中
-                     * 由于服务可能会多次自动启动，该方法可能重复调用
-                     */
-                    @Override
-                    public void onWorking() {
-
-                    }
-                    /**
-                     * 服务终止
-                     * 由于服务可能会被多次终止，该方法可能重复调用，需同onWorking配套使用，如注册和注销broadcast
-                     */
-                    @Override
-                    public void onStop() {
-
-                    }
+        KeepLive.startWork(this,
+            KeepLive.RunMode.ENERGY,
+            foregroundNotification,  //你需要保活的服务，如socket连接、定时任务等，建议不用匿名内部类的方式在这里写
+            object : KeepLiveService {
+                /**
+                 * 运行中
+                 * 由于服务可能会多次自动启动，该方法可能重复调用
+                 */
+                override fun onWorking() {
+                    //  do something
                 }
-        );
+
+                /**
+                 * 服务终止
+                 * 由于服务可能会被多次终止，该方法可能重复调用，需同onWorking配套使用，如注册和注销broadcast
+                 */
+                override fun onStop() {
+                    //   do something
+                }
+            }
+        )
     }
 }
